c663cb809fd5ab32d2c5fdd1407467858a67fef1
astatia <164334619+astatia@users.noreply.github.com>
2024-04-25 15:31:07 +0700

    Add files via upload

---
+++ Latihan Pertemuan 5/latihan1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_6;
+import javax.swing.*;
+/**
+ *
+ * @author Asus
+ */
+public class latihan1 {
+    public static void main(String[] args) { //agar dapat di run sebagai class utama JFrame myWindow;
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My first Java Program");
+        myWindow.setVisible(true);
+        
+        
+    }
+}
